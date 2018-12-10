5a3d4b5eafc9ff0de3ed034a4674faeebcdc7440
ton <panupong.pspd@gmail.com>
2018-12-10 10:12:37 +0700

    Start App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'HelloTon';
-}
+export class AppComponent implements OnInit{
+ 
+  title = 'I Love Thailand';
+
+  // Explicit
+
+  private myTitle: string; //Demo Explicit variable Type String
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+   this.myTitle = "Doramon";
+
+  }
+
+
+
+
+}// App Component Class
+
+
+
+
+
